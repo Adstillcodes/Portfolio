8d739b0fd99be7d04cb0e81a061ef9f64bf45eb1
AD <anvit5.deshpande@gmail.com>
2026-02-26 23:53:08 +0530

    fix:contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Hero />
       <Services />
       <Projects />
-      <Route path="/contact" element={<Contact />} />
+      
     </div>
   );
 }
