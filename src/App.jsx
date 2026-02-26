187389bac9d428ce91cde880f89018531537ee6a
AD <anvit5.deshpande@gmail.com>
2026-02-26 23:42:24 +0530

    add:contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Services />
       <Projects />
+      <Contact />
     </div>
   );
 }
